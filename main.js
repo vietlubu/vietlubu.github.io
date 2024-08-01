a22ea983b97c0979f4575777b356dba33bd04f6b
vietlubu <baoviet3010@gmail.com>
2024-08-01 08:43:39 +0700

    init page

---
+++ main.js
@@ -0,0 +1,14 @@
+$(function () {
+  var options = {
+    strings: ["Coding", "Gaming", "Sitting?", "Sleeping", "Reading", "Writing", "Doing what I love"],
+    typeSpeed: 120,
+    loop: true,
+    startDelay: 500,
+    backDelay: 500,
+    backSpeed: 200
+  }
+
+
+  var typed = new Typed(".type", options);
+
+})
